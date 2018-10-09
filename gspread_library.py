55af6a094dcffddae2a9ad290b3b5c94e084a03c
Davis Keene <dbkeene.tsyc@gmail.com>
2018-10-09 08:32:46 -0400

    Checks 1440 times per day

--- gspread_library.py
+++ gspread_library.py
@@ -1,6 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+import time
 import matplotlib.pyplot as plt
 import numpy as np
 # Constants and Sheets
@@ -10,8 +11,15 @@ scope = ['https://spreadsheets.google.com/feeds',
 credentials = ServiceAccountCredentials.from_json_keyfile_name('DailyPoll-ae4889da60e5.json', scope)
 gc = gspread.authorize(credentials)
 
-responses = gc.open("QOTD Responses").sheet1
-data = pd.DataFrame(responses.get_all_records())
-vals = data['Response'].value_counts()
+string = ""
 
-print("{} currently has {} votes. \n{} currently has {} votes.".format(vals.index[0],vals[0],vals.index[1],vals[1]))
+while True:
+    responses = gc.open("QOTD Responses").sheet1
+    data = pd.DataFrame(responses.get_all_records())
+    vals = data['Response'].value_counts()
+    str = "{} currently has {} votes. \n{} currently has {} votes.".format(vals.index[0], vals[0], vals.index[1],
+                                                                           vals[1])
+    if(str != string):
+        string = str
+        print(string)
+    time.sleep(60) # Updates 1440 times per day
